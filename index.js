9441a62de315efec1ce5dd6feccd19f82d1331ac
riqi <riqi.asdf@gmail.com>
2023-08-15 19:35:34 +0700

    fix

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const port = 8000
 const render = require('./render')
 
 app.get('/:id', (req, res) => {
-  const buffer = render(req.params.id)
+  const buffer = render.main(req.params.id)
+  res.contentType('image/png')
+  res.send(buffer)
+})
+
+app.get('/gif/:id', (req, res) => {
+  const buffer = render.gif(req.params.id)
   res.contentType('image/png')
   res.send(buffer)
 })
 
 app.get('/:id/:amp', (req, res) => {
-  const buffer = render(req.params.id, req.params.amp)
+  const buffer = render.main(req.params.id, req.params.amp)
   res.contentType('image/png')
   res.send(buffer)
 })
