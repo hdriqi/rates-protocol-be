a01335d3cde2624c6aadf0775e6981d0a1db0d69
hdriqi <riqi.asdf@gmail.com>
2023-08-18 18:30:35 +0700

    fix

--- index.js
+++ index.js
@@ -3,50 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 8000
-const render = require('./render')
+const port = 8001
 
 const { MongoClient } = require('mongodb')
 
 const client = new MongoClient(process.env.MONGO_URI)
 
 app.use(cors())
-
-app.get('/render/:id', async (req, res) => {
-  const data = await client.db('rates-protocol').collection('planets').findOne({ nft_id: parseInt(req.params.id) })
-
-  const buffer = render.main({
-    seed: data.digest,
-    id: data.nft_id,
-    px: data.x,
-    py: data.y,
-    rts: data.rts,
-    mrts: data.mrts,
-    prts: data.prts,
-    arts: data.arts
-  })
-
-  res.contentType('image/png')
-  res.send(buffer)
-})
-
-app.get('/planets/:id', async (req, res) => {
-  const data = await client.db('rates-protocol').collection('planets').findOne({ nft_id: parseInt(req.params.id) })
-
-  const buffer = render.main({
-    seed: data.digest,
-    id: data.nft_id,
-    px: data.x,
-    py: data.y,
-    rts: data.rts,
-    mrts: data.mrts,
-    prts: data.prts,
-    arts: data.arts
-  })
-
-  res.contentType('image/png')
-  res.send(buffer)
-})
 
 app.get('/planets', async (req, res) => {
   const sort = req.query.sort ? {
@@ -60,16 +23,9 @@ app.get('/planets', async (req, res) => {
   return res.json(data)
 })
 
-app.get('/gif/:id', (req, res) => {
-  const buffer = render.gif(req.params.id)
-  res.contentType('image/png')
-  res.send(buffer)
-})
-
-app.get('/:id/:amp', (req, res) => {
-  const buffer = render.main(req.params.id, req.params.amp)
-  res.contentType('image/png')
-  res.send(buffer)
+app.get('/planets/:id', async (req, res) => {
+  const data = await client.db('rates-protocol').collection('planets').findOne({ nft_id: parseInt(req.params.id) })
+  res.json(data)
 })
 
 app.listen(port, () => {
